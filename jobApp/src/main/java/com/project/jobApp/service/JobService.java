75ff4e3a5a10c313da6d8f5a18cfe857bf000571
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2025-10-04 12:16:41 +0530

    Java BackEnd

---
+++ jobApp/src/main/java/com/project/jobApp/service/JobService.java
@@ -0,0 +1,35 @@
+package com.project.jobApp.service;
+
+import com.project.jobApp.model.JobPost;
+import com.project.jobApp.repo.JobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class JobService {
+
+    @Autowired
+    public JobRepo repo;
+
+    public void addJob(JobPost jobPost){
+        repo.addJob(jobPost);
+    }
+
+    public List<JobPost> getAllJobs(){
+        return repo.getAllJobs();
+    }
+
+    public JobPost getJob(int postId) {
+
+        return repo.getJob(postId);
+    }
+
+    public void updateJob(JobPost jobPost) {
+        repo.updateJob(jobPost);
+    }
+
+    public void deleteJob(int postId) {
+        repo.deleteJob(postId);
+    }
+}
